b5aa7fa7c5a21db6b9892b0f0e4468096d03173a
pjosephraj <mailjosephraj@gmail.com>
2024-02-17 23:43:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
+function App () {
+  const [editTab, setEditTab] = useState(true);
+
+  const onClickNavItem = () => {
+    setEditTab((isActive) => {
+      return !isActive;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-wrap">
+        <div className="container">
+          <nav className="es-nav">
+            <ul>
+              <li className={editTab ? 'active' : ''}>
+                <button onClick={onClickNavItem} className="es-nav-item">Edit</button>
+              </li>
+              <li className={editTab ? '' : 'active'}>
+                <button onClick={onClickNavItem} className="es-nav-item">Preview</button>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            {editTab ? (
+              <div className="edit-wrapper">
+                <form action="">
+                  <div className="form-control">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName"/>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName"/>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="designation">Designation</label>
+                    <input type="text" id="designation"/>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input type="phone" id="phoneNumber"/>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="linkedIn">LinkedIn Username</label>
+                    <input type="text" id="linkedIn"/>
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="linkedInURL">LinkedIn Link</label>
+                    <input type="text" id="linkedInURL"/>
+                  </div>
+                </form>
+              </div>
+            ) : (
+              <div className="preview wrapper">
+                Preview Wrapper
+              </div>
+            ) }
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
